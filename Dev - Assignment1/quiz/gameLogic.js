b8ec649ac189234ec8668bcd79a4a8b6b9bfad6e
KeshavMakkad <keshav.23bcs10002@ms.sst.scaler.com>
2024-05-21 21:40:12 +0530

    add progress bar and score

--- Dev - Assignment1/quiz/gameLogic.js
+++ Dev - Assignment1/quiz/gameLogic.js
@@ -2,9 +2,13 @@ export default function startGame(quizData) {
   const questionText = document.querySelector(".quiz-question-text");
   const options = document.querySelectorAll(".quiz-option-text");
   const quizContainer = document.querySelector(".quiz-question-container");
+  const progressText = document.querySelector(".progress-text");
+  const progressBarFill = document.querySelector(".progress-bar-fill");
+  const scoreText = document.querySelector(".score");
 
   let score = 0;
   const totalQuestions = quizData.length;
+  let currentQuestionNumber = 0;
 
   options.forEach((option) => {
     option.addEventListener("click", (e) => checkAnswer(e));
@@ -17,6 +21,9 @@ export default function startGame(quizData) {
       endGame();
       return;
     }
+
+    currentQuestionNumber++;
+    updateProgress();
 
     const currQuestionIndex = getRandomIndex();
     const currQuestion = quizData[currQuestionIndex];
@@ -40,7 +47,7 @@ export default function startGame(quizData) {
     const selectedOption = e.target.textContent;
 
     if (selectedOption === currQuestion.correctAnswer) {
-      score++;
+      score += 10;
       e.target.style.backgroundColor = "green";
       e.target.style.color = "white";
     } else {
@@ -48,8 +55,8 @@ export default function startGame(quizData) {
       e.target.style.color = "white";
     }
 
+    scoreText.textContent = `Score: ${score}`;
     quizData.splice(currQuestionIndex, 1);
-
     setTimeout(() => {
       loadQuestion();
     }, 1000);
@@ -59,12 +66,18 @@ export default function startGame(quizData) {
     return Math.floor(Math.random() * quizData.length);
   }
 
+  function updateProgress() {
+    const progress = (currentQuestionNumber / totalQuestions) * 100;
+    progressBarFill.style.width = `${progress}%`;
+    progressText.textContent = `Question ${currentQuestionNumber}/${totalQuestions}`;
+  }
+
   function endGame() {
     quizContainer.innerHTML = `
-        <div class="quiz-end">
-          <h2>Quiz Completed!</h2>
-          <p>Your score is ${score} out of ${totalQuestions}</p>
-        </div>
-      `;
+      <div class="quiz-end">
+        <h2>Quiz Completed!</h2>
+        <p>Your score is ${score} out of ${totalQuestions * 10}</p>
+      </div>
+    `;
   }
 }
